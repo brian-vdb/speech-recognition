24c523d593e999ff5f0e98a117fc83f8caf73a78
brian-vdb <brianvdberg2107@gmail.com>
2023-11-21 12:25:44 +0100

    Fixed syntax

--- main.py
+++ main.py
@@ -16,4 +16,4 @@ input_folder = sys.argv[1] if len(sys.argv) >= 2 else 'input'
 # Handle all of the input files
 filenames = os.listdir(input_folder)
 for filename in filenames:
-    print(filename)
+    print(filenames)
