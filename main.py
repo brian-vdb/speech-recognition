67dc418c2c133c118de96a73345708452963e937
brian-vdb <brianvdberg2107@gmail.com>
2023-11-21 12:14:16 +0100

    Client

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 from dotenv import load_dotenv
-
-# Load variables from the .env file into the environment
-load_dotenv()
+from rev_ai import apiclient
 
 # Get the API key
+load_dotenv()
 api_key = os.environ.get("API_KEY")
+
+# create your client
+client = apiclient.RevAiAPIClient(api_key)
