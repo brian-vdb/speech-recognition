c307c8b5df01dc1fa37fdb2fbd815d8371026bd8
brian-vdb <brianvdberg2107@gmail.com>
2023-11-21 12:25:24 +0100

    Loop through filenames

--- main.py
+++ main.py
@@ -12,3 +12,8 @@ client = apiclient.RevAiAPIClient(api_key)
 
 # Define the input folder from args when available
 input_folder = sys.argv[1] if len(sys.argv) >= 2 else 'input'
+
+# Handle all of the input files
+filenames = os.listdir(input_folder)
+for filename in filenames:
+    print(filename)
