333452a54a863daeae5dfc6f32cb75f257c4d7a4
brian-vdb <brianvdberg2107@gmail.com>
2023-11-21 12:21:14 +0100

    Created input folder

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from rev_ai import apiclient
 
@@ -8,3 +9,6 @@ api_key = os.environ.get("API_KEY")
 
 # create your client
 client = apiclient.RevAiAPIClient(api_key)
+
+# Define the input folder from args when available
+input_folder = sys.argv[1] if len(sys.argv) >= 2 else 'input'
