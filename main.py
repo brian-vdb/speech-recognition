737a55afe41a4021b0045bd0d21fa39939ef7d34
brian-vdb <brianvdberg2107@gmail.com>
2023-11-21 13:11:35 +0100

    Working client handler

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import sys
 from dotenv import load_dotenv
-from rev_ai import apiclient
+from rev_ai import apiclient, JobStatus
+from time import sleep
 
 # Get the API key
 load_dotenv()
@@ -10,10 +11,25 @@ api_key = os.environ.get("API_KEY")
 # create your client
 client = apiclient.RevAiAPIClient(api_key)
 
-# Define the input folder from args when available
-input_folder = sys.argv[1] if len(sys.argv) >= 2 else 'input'
+# Function to process an audio file and get the job id
+def process_audio_file(path: str) -> any:
+    # send a local file
+    job = client.submit_job_local_file(path)
 
-# Handle all of the input files
-filenames = os.listdir(input_folder)
-for filename in filenames:
-    print(filenames)
+    # check job status
+    while(True):
+        if(client.get_job_details(job.id).status != JobStatus.IN_PROGRESS):
+            break
+
+    # return the job id
+    return job.id
+
+if __name__ == "__main__":
+    # Define the input folder from args when available
+    input_folder = sys.argv[1] if len(sys.argv) >= 2 else 'input'
+
+    # Handle all of the input files
+    filenames = os.listdir(input_folder)
+    for filename in filenames:
+        # Use os.path.join to create the full path for each file
+        path = os.path.join(input_folder, filename)
