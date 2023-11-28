Removed incorrect values and tested wer

analyze_build.py
@@ -22,17 +22,21 @@ def calculate_wer(reference, hypothesis):
     # Split the reference and hypothesis sentences into words
     ref_words = reference.split()
     hyp_words = hypothesis.split()
+
     # Initialize a matrix with size |ref_words|+1 x |hyp_words|+1
     # The extra row and column are for the case when one of the strings is empty
     d = np.zeros((len(ref_words) + 1, len(hyp_words) + 1))
+
     # The number of operations for an empty hypothesis to become the reference
     # is just the number of words in the reference (i.e., deleting all words)
     for i in range(len(ref_words) + 1):
         d[i, 0] = i
+
     # The number of operations for an empty reference to become the hypothesis
     # is just the number of words in the hypothesis (i.e., inserting all words)
     for j in range(len(hyp_words) + 1):
         d[0, j] = j
+
     # Iterate over the words in the reference and hypothesis
     for i in range(1, len(ref_words) + 1):
         for j in range(1, len(hyp_words) + 1):
@@ -48,11 +52,12 @@ def calculate_wer(reference, hypothesis):
                 insertion = d[i, j - 1] + 1
                 deletion = d[i - 1, j] + 1
                 d[i, j] = min(substitution, insertion, deletion)
+
     # The minimum number of operations to transform the hypothesis into the reference
     # is in the bottom-right cell of the matrix
     # We divide this by the number of words in the reference to get the WER
     wer = d[len(ref_words), len(hyp_words)] / len(ref_words)
-    return wer, substitution, insertion, deletion
+    return wer
 
 # Function to compare matching texts according to filenames
 def compare_matching_texts(transcript_data: List[Dict[str, str]], audio_mapping: Dict[str, str]) -> None:
@@ -72,11 +77,8 @@ def compare_matching_texts(transcript_data: List[Dict[str, str]], audio_mapping:
             print(f'{transcript_text}\n')
             print(f'{audio_text}\n')
 
-            wer_value, substitution, insertion, deletion  = calculate_wer(transcript_text, audio_text)
+            wer_value = calculate_wer(transcript_text, audio_text)
             print(f'The WER Value: {wer_value}')
-            print(f'Substituted: {substitution}')
-            print(f'Inserted: {insertion}')
-            print(f'Deleted: {deletion}')
         else:
             print(f"No matching entry in audio data for filename: {filename}\n")
 
@@ -85,7 +87,7 @@ def main() -> None:
     transcript_file_path = 'build/transcript_output.json'
 
     # Path to the audio json file
-    audio_file_path = 'build/audio_output.json'
+    audio_file_path = 'build/transcript_output.json'
 
     # Load JSON data
     transcript_data = load_json(transcript_file_path)