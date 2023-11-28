43f6310528d42e2d3fada0fbf839d1315d624c82
brian-vdb <brianvdberg2107@gmail.com>
2023-11-28 12:00:20 +0100

    added functions and main

--- analyze_build.py
+++ analyze_build.py
@@ -1,33 +1,43 @@
 import json
 
-# Path to the transcript json file
-transcript_file_path = 'build/transcript_output.json'
+def load_json(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
 
-# Open the file in read mode
-with open(transcript_file_path, 'r') as file:
-    # Load the JSON data from the file
-    transcript_data = json.load(file)
+def create_audio_mapping(audio_data):
+    return {item['filename']: item['text'] for item in audio_data}
 
-# Path to the audio json file
-audio_file_path = 'build/audio_output.json'
+def compare_matching_texts(transcript_data, audio_mapping):
+    for transcript_entry in transcript_data:
+        filename = transcript_entry['filename']
+        transcript_text = transcript_entry['text']
 
-# Open the file in read mode
-with open(audio_file_path, 'r') as file:
-    # Load the JSON data from the file
-    audio_data = json.load(file)
+        # Check if the filename exists in the audio mapping
+        if filename in audio_mapping:
+            audio_text = audio_mapping[filename]
 
-# Create a dictionary to store 'filename' -> 'text' mapping for the audio data
-audio_mapping = {item['filename']: item['text'] for item in audio_data}
+            # Handle the text from the instances of the same filename
+            print(f'Transcript Text: {transcript_text}\n')
+            print(f'Audio Text: {audio_text}\n')
+        else:
+            print(f"No matching entry in audio data for filename: {filename}\n")
 
-# Iterate through the transcript data
-for transcript_entry in transcript_data:
-    filename = transcript_entry['filename']
-    transcript_text = transcript_entry['text']
+def main():
+    # Path to the transcript json file
+    transcript_file_path = 'build/transcript_output.json'
 
-    # Check if the filename exists in the audio mapping
-    if filename in audio_mapping:
-        audio_text = audio_mapping[filename]
+    # Path to the audio json file
+    audio_file_path = 'build/audio_output.json'
 
-        # Handle the text from the instances of the same filename
-        print(f'{transcript_text}\n')
-        print(f'{audio_text}')
+    # Load JSON data
+    transcript_data = load_json(transcript_file_path)
+    audio_data = load_json(audio_file_path)
+
+    # Create a dictionary to store 'filename' -> 'text' mapping for the audio data
+    audio_mapping = create_audio_mapping(audio_data)
+
+    # Print matching texts
+    compare_matching_texts(transcript_data, audio_mapping)
+
+if __name__ == "__main__":
+    main()
