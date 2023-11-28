0a2612e4733f4413c178e2ba9a29ef7de4cd93e8
Diego-Brandjes <diegobrandjes@hotmail.com>
2023-11-28 12:27:00 +0100

    Added function to calculate WER

--- analyze_build.py
+++ analyze_build.py
@@ -1,4 +1,6 @@
 import json
+import string
+import numpy as np
 
 def load_json(file_path):
     with open(file_path, 'r') as file:
@@ -6,6 +8,12 @@ def load_json(file_path):
 
 def create_audio_mapping(audio_data):
     return {item['filename']: item['text'] for item in audio_data}
+
+def preprocess_text(text:str) -> str:
+    # Remove punctuation from the text
+    text = text.translate(str.maketrans('', '', string.punctuation))
+
+    return text
 
 def compare_matching_texts(transcript_data, audio_mapping):
     for transcript_entry in transcript_data:
@@ -16,11 +24,57 @@ def compare_matching_texts(transcript_data, audio_mapping):
         if filename in audio_mapping:
             audio_text = audio_mapping[filename]
 
+            # Remove punctuation
+            transcript_text = preprocess_text(transcript_text)
+            audio_text = preprocess_text(audio_text)
+
             # Handle the text from the instances of the same filename
-            print(f'Transcript Text: {transcript_text}\n')
-            print(f'Audio Text: {audio_text}\n')
+            print(f'{transcript_text}\n')
+            print(f'{audio_text}\n')
+
+            wer_value, substitution, insertion, deletion  = calculate_wer(transcript_text, audio_text)
+            print(f'The WER Value: {wer_value}')
+            print(f'Substituted: {substitution}')
+            print(f'Inserted: {insertion}')
+            print(f'Deleted: {deletion}')
         else:
             print(f"No matching entry in audio data for filename: {filename}\n")
+
+def calculate_wer(reference, hypothesis):
+    # Split the reference and hypothesis sentences into words
+    ref_words = reference.split()
+    hyp_words = hypothesis.split()
+    # Initialize a matrix with size |ref_words|+1 x |hyp_words|+1
+    # The extra row and column are for the case when one of the strings is empty
+    d = np.zeros((len(ref_words) + 1, len(hyp_words) + 1))
+    # The number of operations for an empty hypothesis to become the reference
+    # is just the number of words in the reference (i.e., deleting all words)
+    for i in range(len(ref_words) + 1):
+        d[i, 0] = i
+    # The number of operations for an empty reference to become the hypothesis
+    # is just the number of words in the hypothesis (i.e., inserting all words)
+    for j in range(len(hyp_words) + 1):
+        d[0, j] = j
+    # Iterate over the words in the reference and hypothesis
+    for i in range(1, len(ref_words) + 1):
+        for j in range(1, len(hyp_words) + 1):
+            # If the current words are the same, no operation is needed
+            # So we just take the previous minimum number of operations
+            if ref_words[i - 1] == hyp_words[j - 1]:
+                d[i, j] = d[i - 1, j - 1]
+            else:
+                # If the words are different, we consider three operations:
+                # substitution, insertion, and deletion
+                # And we take the minimum of these three possibilities
+                substitution = d[i - 1, j - 1] + 1
+                insertion = d[i, j - 1] + 1
+                deletion = d[i - 1, j] + 1
+                d[i, j] = min(substitution, insertion, deletion)
+    # The minimum number of operations to transform the hypothesis into the reference
+    # is in the bottom-right cell of the matrix
+    # We divide this by the number of words in the reference to get the WER
+    wer = d[len(ref_words), len(hyp_words)] / len(ref_words)
+    return wer, substitution, insertion, deletion
 
 def main():
     # Path to the transcript json file
